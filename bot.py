070ae91afca7a9f2c8d93c4b6e73f6e75374e9eb
deadstar <74278992+DeadstarIII@users.noreply.github.com>
2022-07-14 18:25:23 +0530

    Initial commit
    Added hour feature, now you can use hours in the command usage rather than seconds, well time only in hours, not hours mixed with seconds.

--- bot.py
+++ bot.py
@@ -3,37 +3,43 @@ import discord
 from discord.ext import commands
 import asyncio
 from dotenv import load_dotenv
+import datetime
 load_dotenv()
-TOKEN=os.getenv("token")
+TOKEN = os.getenv("token")
 bot = commands.Bot(command_prefix=">", case_sensitive=False)
 bot.remove_command("help")
+
+
 @bot.event
 async def on_ready():
-     print("Bot logged in")
+    print("Bot logged in")
+    # Don't give anything here for changing status from Online to Idle etc, don't give a playing status in `on_ready`, making API calls in on_ready can ban your IP from discord.
+
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def giveaway(ctx,winner:discord.Member,duration,*, msg):
-      await ctx.message.delete()
-      int_dur=int(duration)
-      dur_mins=int_dur/60
-      dur_hrs=dur_mins/60
-      giveaway_embed=discord.Embed()
-      giveaway_embed.title="Giveaway"
-      giveaway_embed.description=f"""{msg}
-      React with :tada: to participate, ends in {dur_hrs} hours
-      """
-      gw_msg=await ctx.send(embed=giveaway_embed)
-      await gw_msg.add_reaction("🎉")
-      await asyncio.sleep(int_dur)
-      winner_embed=discord.Embed()
-      winner_embed.title="Winner"
-      winner_embed.description=f"<@{winner.id}> won **{msg}**"
-      await ctx.send(f"<@{winner.id}>")
-      await ctx.send(embed=winner_embed)
-#use only 'seconds' in the while using the cmd, the time will be given in hrs in embed,
+async def giveaway(ctx, winner: discord.Member, duration_hours, *, msg):
+    await ctx.message.delete()
+    UTC_now = datetime.datetime.now()
+    int_dur = int(float(duration_hours))  # int()
+    dur_mins = int_dur*60
+    dur_seconds = dur_mins*60
+    giveaway_embed = discord.Embed()
+    giveaway_embed.title = f"Giveaway of {msg}"
+    giveaway_embed.description = f"""React with :tada: to participate, ends in {duration_hours} hours. This giveaway started at {UTC_now.year}-{UTC_now.month}-{UTC_now.day} \t {UTC_now.hour}:{UTC_now.minute}
+      """  # UTC_now.second
+    gw_msg = await ctx.send(embed=giveaway_embed)
+    await gw_msg.add_reaction("🎉")
+    await asyncio.sleep(dur_seconds)
+    winner_embed = discord.Embed()
+    winner_embed.title = "Winner"
+    winner_embed.description = f"<@{winner.id}> won **{msg}**"
+    await ctx.send(f"<@{winner.id}>")
+    await ctx.send(embed=winner_embed)
+
+# use only 'seconds' in the while using the cmd, the time will be given in hrs in embed,
 # example
-# >giveaway @user(the winner) 18000(5.0 hrs, duration in seconds only) Nitro Classic (msg is the prize)
-      
+# >giveaway @user(the winner) 5 (5.0 hrs, duration in hours only) Nitro Classic ("msg" is the prize)
+
 
 bot.run(TOKEN)
